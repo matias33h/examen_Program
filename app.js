0f0df966b3682ab390379145f9fbe38e5aaa0b89
matias33h <matiasortizggg@gmail.com>
2022-10-12 09:08:57 -0300

    4 commit

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
+
 const path=require('path')
 const express= require('express')
 const cors =require('cors')
+const app=express()
 
 const conexion = require('./db/database')
 conexion();
 
-const app=express()
-require('dotenv').config();
-//configuraciones
-puerto=process.env.PORT || 3000
-
-
-// MIDDLEWARES
-app.use(cors());
-app.use(morgan('dev'));
 app.use(express.json())
 
 app.use(require("./routes/tareas.routes"))
 app.use(require("./routes/user.routes"))
 
 
+puerto=process.env.PORT || 3000
 
 app.listen(puerto,()=>{
 console.log(`se inicio el servidor en http://localhost:${puerto}`)
 })
+
+
+
+
+
+
